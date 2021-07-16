d9303ef9fea7425539996d612e8e639cb0a67227
shahsakshi0403 <sakshishah0403@gmail.com>
2021-07-16 23:13:35 +0530

    16/07/21

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import { Switch ,Route } from 'react-router-dom';
 
-
+const HatsPage =() =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage/>
+      <switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+        {/* <Route path='/' component={HomePage}></Route> */}
+      </switch>
     </div>
   );
 }

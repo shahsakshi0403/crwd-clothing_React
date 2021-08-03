179f9115c03d91e7b045aaa72b915216e4953b04
shahsakshi0403 <sakshishah0403@gmail.com>
2021-08-03 21:03:56 +0530

    03/08/21

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,11 @@
-import React from 'react';
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-import SHOP_DATA from './shop.data.js';
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
 
 export default ShopPage;

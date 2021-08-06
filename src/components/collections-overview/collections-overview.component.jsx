78c83a694cba784d3ce9e5a4658e4fe58c4762de
shahsakshi0403 <sakshishah0403@gmail.com>
2021-08-06 21:10:32 +0530

    06/08/21

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './collections-overview.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
-import { selectCollections } from "../../redux/shop/shop.selector";
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
+
+import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-    <div className='collections-overview'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-    </div>
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections : selectCollections
-}); 
+  collections: selectCollectionsForPreview
+});
 
-export default connect(mapStateToProps)(CollectionsOverview); 
+export default connect(mapStateToProps)(CollectionsOverview);

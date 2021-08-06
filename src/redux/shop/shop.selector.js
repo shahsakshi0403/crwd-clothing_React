a225d345ba1e6d0dde272ff608a73410afbd4944
shahsakshi0403 <sakshishah0403@gmail.com>
2021-08-06 20:38:29 +0530

    06/08/21

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,14 +2,6 @@ import { createSelector } from "reselect";
 
 const selectShop = state => state.shop;
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-};
-
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
@@ -18,6 +10,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam =>
     createSelector(
         [selectCollections],
-        collection => collection.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-        )
+        collections => collections[collectionUrlParam]
     );

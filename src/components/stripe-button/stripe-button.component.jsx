786f942213b0d7c32614227fc5fdb9a98b07bc6d
shahsakshi0403 <sakshishah0403@gmail.com>
2021-08-07 14:48:27 +0530

    07/08/21

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51JLkvRSA86xZNdA2eoztU7Y6leXFRNqJ9OuQgMDLjZ4asEVi8eZPW1ECBtriykblHADrpgfjdAGRhQPVSbywPPfg00TCBLYvo2';
+
+    const onToken = token => {
+        alert('Payment Successful!')
+        console.log('Payment Successful!')
+    }
+
+    return(
+        <StripeCheckout 
+            label ='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image = 'https://svgshare.com/i/CUz.svg'
+            description = {`Your Total is $${price}`}
+            amount={priceForStripe}
+            panelLabel = 'Pay Now'
+            token = {onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;

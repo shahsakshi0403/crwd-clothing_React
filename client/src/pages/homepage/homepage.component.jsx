cfd6d26716fb303136de4fba9898495bbc245052
shahsakshi0403 <sakshishah0403@gmail.com>
2021-08-30 23:22:55 +0530

    30/08/21

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -5,11 +5,9 @@ import Directory from '../../components/directory/directory.component';
 // import './homepage.styles.scss';
 import { HomePageContainer } from './homepage.styles';
  const HomePage = () => (
-//   <div className='homepage'>
     <HomePageContainer>
      <Directory />
     </HomePageContainer>
-//   </div>
 );
 
 export default HomePage;
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,38 +1,45 @@
-import React , { useEffect } from "react";
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect, lazy, Suspense } from "react";
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+import Spinner from "../../components/spinner/spinner.component";
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container';
-import CollectionPageContainer  from '../collection/collection.container';
+// import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container';
+// import CollectionPageContainer  from '../collection/collection.container';
 
-const ShopPage = ({ match , fetchCollectionsStart }) => {
+const CollectionsOverviewContainer = lazy(() =>
+  import("../../components/collections-overview/collection-overview.container")
+);
+
+const CollectionPageContainer = lazy(() =>
+  import("../collection/collection.container")
+);
+
+const ShopPage = ({ match, fetchCollectionsStart }) => {
   useEffect(() => {
     fetchCollectionsStart();
   }, [fetchCollectionsStart]);
 
-    return (
-      <div className='shop-page'>
+  return (
+    <Suspense fallback={<Spinner />}>
+      <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
-          component = { CollectionsOverviewContainer}
+          component={CollectionsOverviewContainer}
         />
         <Route
           path={`${match.path}/:collectionId`}
-          component = { CollectionPageContainer }
+          component={CollectionPageContainer}
         />
       </div>
-    );
-  }
+    </Suspense>
+  );
+};
 
-
-const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStart : () => dispatch(fetchCollectionsStart())
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
